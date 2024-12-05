9b6ed825a81728356ebe1c6504aa92932df44006
Inna Khalas <innamakarec349@gmail.com>
2024-12-05 16:03:43 +0400

    add FriendList component

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,11 +1,15 @@
+import './FriendList.css';
+
 export default function FriendList({ friends }) {
     return (
-        <ul>
+        <ul className="friend-list">
             {friends.map(friend => (
-                <li key={friend.id}>
-                    <img src={friend.avatar} alt="Avatar" width="48" />
-                    <p>{friend.name}</p>
-                    <p>{friend.isOnline ? "Online" : "Offline"}</p>
+                <li key={friend.id} className="friend-item">
+                    <img src={friend.avatar} alt="Avatar" className='friend-avatar' />
+                    <p className="friend-name">{friend.name}</p>
+                    <p className={`friend-status ${friend.isOnline ? "online" : "offline"}`}>
+                        {friend.isOnline ? "Online" : "Offline"}
+                    </p>
                 </li>
             ))}
         </ul>
