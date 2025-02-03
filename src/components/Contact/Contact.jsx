622cd14d35c1e70437a97aeafa8c939f643f4962
Inna Khalas <innamakarec349@gmail.com>
2025-02-03 16:09:05 +0400

    rename contacts

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,6 +1,6 @@
 import { useDispatch } from "react-redux";
 import { IoMdCloseCircle } from "react-icons/io";
-import { deleteContact } from "../../redux/contacts/contactsOps";
+import { deleteContact } from "../../redux/contacts/operations";
 
 const Contact = ({ name, number, id }) => {
   const dispatch = useDispatch();
